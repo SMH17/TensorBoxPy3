d9b5217e9c0f840f2d14256a08812f5e88de416b
Silvio Marano <silvio.smh17@yahoo.it>
2017-09-08 18:09:13 +0200

    added extra utils

---
+++ utils/create_meta_graph.py
@@ -0,0 +1,40 @@
+# TensorBoxPy3 https://github.com/SMH17/TensorBoxPy3
+
+import tensorflow as tf
+from train import build_forward
+import cv2
+import argparse
+import random
+import json
+import os
+
+def create_meta_graph(args, H):
+    tf.reset_default_graph()
+    x_in = tf.placeholder(tf.float32, name='x_in', shape=[H['image_height'], H['image_width'], 3])
+    if H['use_rezoom']:
+        pred_boxes, pred_logits, pred_confidences, pred_confs_deltas, pred_boxes_deltas = build_forward(H, tf.expand_dims(x_in, 0), 'test', reuse=None)
+        grid_area = H['grid_height'] * H['grid_width']
+        pred_confidences = tf.reshape(tf.nn.softmax(tf.reshape(pred_confs_deltas, [grid_area * H['rnn_len'], 2])), [grid_area, H['rnn_len'], 2])
+        if H['reregress']:
+            pred_boxes = pred_boxes + pred_boxes_deltas
+    else:
+        pred_boxes, pred_logits, pred_confidences = build_forward(H, tf.expand_dims(x_in, 0), 'test', reuse=None)
+    tf.add_to_collection('placeholders', x_in)
+    tf.add_to_collection('vars', pred_boxes)
+    tf.add_to_collection('vars', pred_confidences)
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        saver.save(sess, args.output)
+        tf.train.write_graph(sess.graph.as_graph_def(), '', "{}.pb".format(args.output))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output', required=True)
+    parser.add_argument('--hypes', required=True)
+    parser.add_argument('--gpu', default=0)
+    args = parser.parse_args()
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
+    with open(args.hypes, 'r') as f:
+        H = json.load(f)
+    create_meta_graph(args, H)
